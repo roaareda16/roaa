2c229291cc710638c6953c47a634a9dca7da7a3e
roaareda16 <148653679+roaareda16@users.noreply.github.com>
2023-11-29 23:07:37 +0200

    add file task 1

---
+++ task1.1.py
@@ -0,0 +1,5 @@
+units = int(input("Please enter units: ").strip())
+tens = int(input("Please enter tens: ").strip())
+handerds = int(input("Please enter hundreds: ").strip())
+
+print(f"The number is:{handerds}{tens}{units}")
